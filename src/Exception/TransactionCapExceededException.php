a3011f7d05104de92ebc16177ced1e3de61ad843
Zach Schneider <hello@zacharyschneider.ca>
2020-09-03 17:05:36 -0700

    Add more exceptions

---
+++ src/Exception/TransactionCapExceededException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Zaxbux\BackblazeB2\Exception;
+
+class TransactionCapExceededException extends \Exception {}
---
+++ src/Exception/UnauthorizedException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Zaxbux\BackblazeB2\Exception;
+
+class UnauthorizedException extends \Exception {}
