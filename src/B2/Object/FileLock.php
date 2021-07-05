d23e2056c5e4712e07771d947b2e5b9e1d5e88c9
Zachary Schneider <hello@zacharyschneider.ca>
2021-07-04 23:47:49 -0700

    Add more B2 objects

---
+++ src/B2/Object/FileLock.php
@@ -0,0 +1,177 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaxbux\BackblazeB2\B2\Object;
+
+use ArrayAccess;
+use JsonSerializable;
+use Zaxbux\BackblazeB2\Traits\ProxyArrayAccessToProperties;
+
+/**
+ * @link https://www.backblaze.com/b2/docs/server_side_encryption.html
+ * @package Zaxbux\BackblazeB2\B2\Object
+ */
+class FileLock implements JsonSerializable, ArrayAccess {
+	use ProxyArrayAccessToProperties;
+
+	public const ATTRIBUTE_IS_CLIENT_AUTHORIZED_TO_READ = 'isClientAuthorizedToRead';
+	public const ATTRIBUTE_RETENTION_MODE               = 'mode';
+	public const ATTRIBUTE_RETENTION_PERIOD             = 'retainUntilTimestamp';
+	public const ATTRIBUTE_LEGAL_HOLD                   = 'legalHold';
+
+	public const HEADER_FILE_RETENTION_MODE   = 'X-Bz-File-Retention-Mode';
+	public const HEADER_FILE_RETENTION_PERIOD = 'X-Bz-File-Retention-Retain-Until-Timestamp';
+	public const HEADER_FILE_LEGAL_HOLD       = 'X-Bz-File-Legal-Hold';
+
+	public const FILE_RETENTION_MODE_COMPLIANCE = 'compliance';
+	public const FILE_RETENTION_MODE_GOVERNANCE = 'governance';
+
+	public const LEGAL_HOLD_ENABLED  = 'on';
+	public const LEGAL_HOLD_DISABLED = 'off';
+
+	/** @var array */
+	private $clientAuthorizedToRead = [];
+	
+	/** @var string */
+	private $fileRetentionMode;
+	
+	/** @var int */
+	private $fileRetentionPeriod;
+	
+	/** @var bool */
+	private $legalHold;
+
+	/**
+	 * Get the value of fileRetentionMode
+	 */ 
+	public function getFileRetentionMode(): string
+	{
+		return $this->fileRetentionMode;
+	}
+
+	/**
+	 * Set the value of fileRetentionMode
+	 */ 
+	public function setFileRetentionMode(string $fileRetentionMode): FileLock
+	{
+		$this->fileRetentionMode = $fileRetentionMode;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of fileRetentionPeriod
+	 */ 
+	public function getFileRetentionPeriod(): int
+	{
+		return $this->fileRetentionPeriod;
+	}
+
+	/**
+	 * Set the value of fileRetentionPeriod
+	 */ 
+	public function setFileRetentionPeriod($fileRetentionPeriod): FileLock
+	{
+		$this->fileRetentionPeriod = $fileRetentionPeriod;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of legalHold
+	 */ 
+	public function getLegalHold(): string
+	{
+		return $this->legalHold ? static::LEGAL_HOLD_ENABLED : static::LEGAL_HOLD_DISABLED;
+	}
+
+	/**
+	 * Set the value of legalHold
+	 * @param bool|string $legalHold
+	 */ 
+	public function setLegalHold($legalHold): FileLock
+	{
+		if ($legalHold === static::LEGAL_HOLD_ENABLED || $legalHold === true) {
+			$this->legalHold = static::LEGAL_HOLD_ENABLED;
+		} else if ($legalHold === static::LEGAL_HOLD_DISABLED || $legalHold === false) {
+			$this->legalHold = static::LEGAL_HOLD_DISABLED;
+		}
+
+		return $this;
+	}
+
+	public function __construct(
+		?string $fileRetentionMode = null,
+		?string $fileRetentionPeriod = null,
+		?bool $legalHold = null,
+		?array $clientAuthorizedToRead = []
+	) {
+		$this->fileRetentionMode = $fileRetentionMode;
+		$this->fileRetentionPeriod = $fileRetentionPeriod;
+		$this->legalHold = $legalHold === null ? null : (
+			$legalHold ? static::LEGAL_HOLD_ENABLED : static::LEGAL_HOLD_DISABLED
+		);
+		$this->clientAuthorizedToRead = $clientAuthorizedToRead;
+	}
+	
+	/**
+	 * Get the File Lock configuration as headers understood by the B2 API.
+	 * 
+	 * @return array 
+	 */
+	public function getHeaders(): array
+	{
+		return array_filter([
+			static::HEADER_FILE_RETENTION_MODE   => $this->fileRetentionMode,
+			static::HEADER_FILE_RETENTION_PERIOD => $this->fileRetentionPeriod,
+			static::HEADER_FILE_LEGAL_HOLD       => $this->legalHold,
+		]);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public static function fromArray(array $data): FileLock
+	{
+		return new static(
+			$data[File::ATTRIBUTE_FILE_RETENTION][static::ATTRIBUTE_RETENTION_MODE] ?? $data[static::ATTRIBUTE_RETENTION_MODE] ?? null,
+			$data[File::ATTRIBUTE_FILE_RETENTION][static::ATTRIBUTE_RETENTION_PERIOD] ?? $data[static::ATTRIBUTE_RETENTION_PERIOD] ?? null,
+			$data[static::ATTRIBUTE_LEGAL_HOLD] ?? null,
+			array_filter([
+				$data[File::ATTRIBUTE_FILE_RETENTION][static::ATTRIBUTE_IS_CLIENT_AUTHORIZED_TO_READ] ?
+					File::ATTRIBUTE_FILE_RETENTION : null,
+				$data[File::ATTRIBUTE_LEGAL_HOLD][static::ATTRIBUTE_IS_CLIENT_AUTHORIZED_TO_READ] ?
+					File::ATTRIBUTE_LEGAL_HOLD : null,
+			])
+		);
+	}
+
+	public function isClientAuthorizedToRead($attribute): ?bool {
+		return $this->clientAuthorizedToRead[$attribute] ?? null;
+	}
+
+	public function toArray(): array
+	{
+		return [
+			File::ATTRIBUTE_FILE_RETENTION => [
+				static::ATTRIBUTE_IS_CLIENT_AUTHORIZED_TO_READ => null,
+				'value' => $this->isClientAuthorizedToRead(File::ATTRIBUTE_LEGAL_HOLD) ? [
+					static::ATTRIBUTE_RETENTION_MODE   => $this->fileRetentionMode,
+					static::ATTRIBUTE_RETENTION_PERIOD => $this->fileRetentionPeriod,
+				] : null,
+			],
+			File::ATTRIBUTE_LEGAL_HOLD => [
+				static::ATTRIBUTE_IS_CLIENT_AUTHORIZED_TO_READ => null,
+				'value' => $this->isClientAuthorizedToRead(File::ATTRIBUTE_LEGAL_HOLD) ?
+					($this->legalHold ? static::LEGAL_HOLD_ENABLED : static::LEGAL_HOLD_DISABLED) : null,
+			],
+		];
+	}
+
+	/** @inheritdoc */
+	public function jsonSerialize()
+	{
+		return $this->toArray();
+	}
+}
---
+++ src/Classes/LargeFileUpload.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Zaxbux\BackblazeB2\Classes;
+
+use RuntimeException;
+use Zaxbux\BackblazeB2\Client;
+use Zaxbux\BackblazeB2\B2\Object\File;
+use Zaxbux\BackblazeB2\B2\Object\FileInfo;
+use Zaxbux\BackblazeB2\B2\Object\ServerSideEncryption;
+
+
+class LargeFileUpload {
+	private $client;
+	private $stream;
+	private $fileName;
+
+	private $contentLength;
+	private $contentType;
+	private $contentSha1;
+
+	private $fileRetention;
+	private $legalHoldStatus;
+	private $serverSideEncryption;
+
+	private $file;
+
+	private $uploadPartUrl;
+
+	private $bytesSent = 0;
+	private $partCount = 0;
+
+	/** @var array */
+	private $partSha1Array = [];
+
+	public function __construct(Client $client)
+	{
+		$this->client = $client;
+	}
+
+	public static function create(Client $client) {
+		return new static($client);
+	}
+
+	public function withStream(
+		$stream,
+		string $fileName,
+	): LargeFileUpload {
+		$this->stream = $stream;
+		$this->fileName = $fileName;
+
+		return $this;
+	}
+
+	public function useContentType(string $contentType): LargeFileUpload
+	{
+		$this->contentType = $contentType;
+		return $this;
+	}
+
+	public function useFileMetadata(FileUploadMetadata $metadata): LargeFileUpload
+	{
+		$this->contentLength = $metadata->getLength();
+		$this->contentSha1 = $metadata->getSha1();
+		return $this;
+	}
+
+	public function useFileInfo(FileInfo $fileInfo): LargeFileUpload
+	{
+		$this->fileInfo = $fileInfo;
+		return $this;
+	}
+
+	public function legalHold(?bool $enabled = true): LargeFileUpload
+	{
+		$this->legalHold = $enabled ? 'on' : 'off';
+
+		return $this;
+	}
+
+	public function withFileRetention(array $fileRetention): LargeFileUpload
+	{
+		$this->fileRetention = $fileRetention;
+
+		return $this;
+	}
+
+	public function withEncryption(ServerSideEncryption $sse = null): LargeFileUpload
+	{
+		$this->serverSideEncryption = $sse ?? new ServerSideEncryption();
+
+		return $this;
+	}
+
+	public function start(string $bucketId): LargeFileUpload
+	{
+		if (!$this->stream) {
+			throw new RuntimeException('Missing file pointer or stream.');
+		}
+
+		if (!$this->fileName) {
+			throw new RuntimeException('File name not set.');
+		}
+
+		if (!$this->metadata) {
+			$this->metadata = FileUploadMetadata::fromResource($this->stream);
+		}
+
+		if ($this->metadata->getLength() < $this->minimumPartSize()) {
+			throw new RuntimeException('Upload size is less than absolute minimum part size.');
+		}
+
+		if ($this->metadata->getLength() > File::LARGE_FILE_MAX_SIZE) {
+			throw new RuntimeException('Upload size exceeds large file limit.');
+		}
+
+		// Start large file
+		$this->file = $this->client->startLargeFile(
+			$bucketId,
+			$this->fileName,
+			$this->contentType,
+			$this->fileInfo,
+			$this->fileRetention,
+			$this->legalHoldStatus,
+			$this->serverSideEncryption
+		);
+
+		// Get upload part url
+		$this->uploadPartUrl = $this->client->getUploadPartUrl($this->file->getId());
+
+		return $this;
+	}
+
+	public function uploadParts() {
+		while ($this->remainingBytes() > 0) {
+			$this->partCount++;
+			$offset = $this->bytesToSend();
+
+			$partString = fread($this->stream, $offset);
+			
+			$metadata = FileUploadMetadata::fromResource($partString);
+			array_push($this->partSha1Array, $metadata->getSha1());
+
+			$part = $this->client->uploadPart(
+				$partString,
+				$this->file->getId(),
+				$this->partCount,
+				$this->serverSideEncryption,
+				$this->uploadPartUrl,
+				$metadata
+			);
+
+			$this->bytesSent += $offset;
+		}
+
+		return $this;
+	}
+
+	public function finish() {
+		// Finish large file
+
+		// Get file info to confirm
+		return $this;
+	}
+
+	public function getFile() {
+		return $this->file;
+	}
+
+	private function minimumPartSize(): int
+	{
+		return $this->client->getAccountAuthorization()->getAbsoluteMinimumPartSize();
+	}
+
+	private function recommendedPartSize(): int
+	{
+		return $this->client->getAccountAuthorization()->getRecommendedPartSize();
+	}
+
+	private function remainingBytes(): int
+	{
+		return $this->contentLength - $this->bytesSent;
+	}
+
+	private function bytesToSend(): int
+	{
+		// Return the remaining byte size if less than the minimum part size (should be the last part of the file to upload)
+		if ($this->remainingBytes() < $this->minimumPartSize()) {
+			return $this->remainingBytes();
+		}
+		
+		return $this->recommendedPartSize();
+	}
+}
