399567bcd6b7f8f247f9aa5e46fe402411dd717c
Zachary Schneider <hello@zacharyschneider.ca>
2021-07-08 23:48:28 -0700

    todo, constants

--- src/Object/File/FileInfo.php
+++ src/Object/File/FileInfo.php
@@ -14,6 +14,7 @@ use Zaxbux\BackblazeB2\Classes\AbstractObjectInfo;
  */
 final class FileInfo extends AbstractObjectInfo {
 	public const B2_FILE_INFO_MTIME = 'src_last_modified_millis';
+	public const B2_FILE_INFO_LARGE_FILE_SHA1 = 'large_file_sha1';
 
 	public static function fromArray(array $data): FileInfo {
 		return new FileInfo($data);
